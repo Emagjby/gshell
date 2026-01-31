a47d4317f27140d55d8aca75bda07b3c8751a324
Emagjby <emagjby@gmail.com>
2026-01-31 19:43:34 +0200

    feat: read line and primitive error handling

--- src/main.c
+++ src/main.c
@@ -4,7 +4,31 @@
 
 int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
-  write(1, "$ ", 2);
+
+  // Display prompt
+  char prompt[2] = {'$', ' '};
+  write(1, prompt, sizeof(prompt));
+
+  // Read user input & remove \n
+  char *command = NULL;
+  size_t cap = 0;
+
+  ssize_t read = getline(&command, &cap, stdin);
+  if (read > 0 && command[read - 1] == '\n') {
+    command[read - 1] = '\0';
+    read--;
+  }
+  
+  // Error handling
+  char buf[128];
+  int len = snprintf(buf, sizeof(buf), "%s: command not found", command);
+  write(1, buf, len);
+
+  // Free allocated memory
+  free(command);
+
+  // Print newline before exiting
+  write(2, "\n", 1);
 
   return 0;
 }
