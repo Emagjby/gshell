f4172bb6fa4ded8e218acba69a7a8dff7276191c
Emagjby <emagjby@gmail.com>
2026-02-06 13:44:33 +0200

    feat(autocomplete): autocomplete command only if it's first

--- src/linenoise_setup.c
+++ src/linenoise_setup.c
@@ -1,25 +1,44 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "linenoise.h"
 #include "dynbuf.h"
 #include "linenoise_setup.h"
 #include "helpers.h"
 
-#include <string.h>
-
 static void completion_callback(const char* buf, linenoiseCompletions* lc) {
-    const char* token = strrchr(buf, ' ');
-    token = token ? token + 1 : buf;
+    // Skip leading spaces
+    const char* start = buf;
+    while(*start == ' ') start++;
+
+    const char* token_start = strrchr(start, ' ');
+    const char* token = token_start ? token_start + 1 : start;
+
+    int is_first_token = (token_start == NULL);
+
+    size_t prefix_len = token - buf;
+    char* prefix = strndup(buf, prefix_len);
+
 
     for(int i = 0; builtins[i]; i++) {
-        if(strncmp(token, builtins[i], strlen(token)) == 0) {
-            DynBuf dynbuf;
-            dynbuf_init(&dynbuf);
-            dynbuf_append(&dynbuf, builtins[i]);
-            dynbuf_append(&dynbuf, " ");
+        if(is_first_token) {
+            if(strncmp(token, builtins[i], strlen(token)) == 0) {
+                DynBuf dynbuf;
+                dynbuf_init(&dynbuf);
+                
+                dynbuf_append(&dynbuf, prefix);
 
-            linenoiseAddCompletion(lc, dynbuf.buf);
-            dynbuf_free(&dynbuf);
-        }
+                dynbuf_append(&dynbuf, builtins[i]);
+                dynbuf_append(&dynbuf, " ");
+
+                linenoiseAddCompletion(lc, dynbuf.buf);
+                dynbuf_free(&dynbuf);
+            }
+        } 
     }
+
+    free(prefix);
 }
 
 void repl_linenoise_init(void) {
